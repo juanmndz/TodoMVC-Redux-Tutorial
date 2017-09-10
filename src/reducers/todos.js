0f5d9e2d76df37533890459ed5af43d69d172ed3
Juan Mendez <juanmndz19@gmail.com>
2017-09-09 22:22:21 -0700

    Finalize application functions

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,4 +1,12 @@
-import { ADD_TODO, DELETE_TODO, EDIT_TODO, COMPLETE_TODO } from '../constants/ActionTypes';
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  EDIT_TODO,
+  COMPLETE_TODO,
+  COMPLETE_ALL,
+  CLEAR_COMPLETED,
+} from '../constants/ActionTypes';
+
 const initialState = [
   {
     text: 'Use Redux',
@@ -18,12 +26,15 @@ export default function todos(state = initialState, action) {
           text: action.text,
         },
       ];
+
     case DELETE_TODO:
       return state.filter(todo => todo.id !== action.id);
+
     case EDIT_TODO:
       return state.map(
         todo => (todo.id === action.id ? { ...todo, text: action.text } : todo),
       );
+
     case COMPLETE_TODO:
       return state.map(
         todo =>
@@ -31,6 +42,16 @@ export default function todos(state = initialState, action) {
             ? { ...todo, completed: !todo.completed }
             : todo,
       );
+
+    case COMPLETE_ALL:
+      const areAllMarked = state.every(todo => todo.completed);
+      return state.map(todo => ({
+        ...todo,
+        completed: !areAllMarked,
+      }));
+
+    case CLEAR_COMPLETED:
+      return state.filter(todo => todo.completed === false);
 
     default:
       return state;
